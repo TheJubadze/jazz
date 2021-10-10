5982a153f408c0487c5354d9ba86d80321cea8ac
Vitaly Senokosov <6545415@gmail.com>
2021-10-10 22:51:37 +0300

    Remove submodules

--- Jazz/src/Jazz/EntryPoint.h
+++ Jazz/src/Jazz/EntryPoint.h
@@ -2,6 +2,9 @@
 
 int main(int, char **) {
     Jazz::Log::Init();
+    Jazz::Log::GetCoreLogger()->warn("Initialized Log!");
+    Jazz::Log::GetClientLogger()->info("Hello Jazz!");
+
     auto app = Jazz::CreateApplication();
     app->Run();
     delete app;
