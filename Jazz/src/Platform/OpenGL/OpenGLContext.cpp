9354c661531f510f7d8fb3a5550074bc6e8d8b7c
Vitaly <6545415@gmail.com>
2021-11-13 23:22:18 +0300

    render first triangle!

--- Jazz/src/Platform/OpenGL/OpenGLContext.cpp
+++ Jazz/src/Platform/OpenGL/OpenGLContext.cpp
@@ -14,6 +14,11 @@ namespace Jazz {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
         JZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        JZ_CORE_INFO("OpenGL Info:");
+        JZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+        JZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+        JZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
     }
 
     void OpenGLContext::SwapBuffers() {
