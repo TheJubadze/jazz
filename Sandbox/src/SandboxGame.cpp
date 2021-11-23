253094517f9ee501321777150798dc4ac4c53fe8
Vitaly Senokosov <vitalyse@playtika.com>
2021-11-23 17:48:36 +0300

    mov OpenGL shader implementation to OpenGLShader class + add more uniform functions

--- Sandbox/src/SandboxGame.cpp
+++ Sandbox/src/SandboxGame.cpp
@@ -1,7 +1,8 @@
 #include <Jazz.h>
-#include <imgui.h>
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <Platform/OpenGL/OpenGLShader.h>
 
 class ExampleLayer : public Jazz::Layer {
 public:
@@ -86,10 +87,9 @@ public:
 	}
 		)";
 
-        m_Shader = std::make_unique<Jazz::Shader>(vertexSrc, fragmentSrc);
+        m_Shader.reset(Jazz::Shader::Create(vertexSrc, fragmentSrc));
 
-
-        std::string blueShaderVertexSrc = R"(
+        std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -106,20 +106,22 @@ public:
 			}
 		)";
 
-        std::string blueShaderFragmentSrc = R"(
+        std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+            uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+                color = vec4(u_Color, 1.0);
 			}
 		)";
 
-        m_BlueShader = std::make_shared<Jazz::Shader>(blueShaderVertexSrc, blueShaderFragmentSrc);
+        m_FlatColorShader.reset(Jazz::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
     }
 
@@ -149,11 +151,14 @@ public:
 
         glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+        std::dynamic_pointer_cast<Jazz::OpenGLShader>(m_FlatColorShader)->Bind();
+        std::dynamic_pointer_cast<Jazz::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
         for (int y = 0; y < 20; y++) {
             for (int x = 0; x < 20; x++) {
                 glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
                 glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-                Jazz::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+                Jazz::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
             }
         }
 
@@ -167,13 +172,16 @@ public:
     }
 
     void OnImGuiRender() override {
+        ImGui::Begin("Settings");
+        ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+        ImGui::End();
     }
 
 private:
     std::shared_ptr<Jazz::Shader> m_Shader;
     std::shared_ptr<Jazz::VertexArray> m_VertexArray;
 
-    std::shared_ptr<Jazz::Shader> m_BlueShader;
+    std::shared_ptr<Jazz::Shader> m_FlatColorShader;
     std::shared_ptr<Jazz::VertexArray> m_SquareVA;
 
     Jazz::OrthographicCamera m_Camera;
@@ -182,6 +190,8 @@ private:
 
     float m_CameraRotation = 0.0f;
     float m_CameraRotationSpeed = 180.0f;
+
+    glm::vec3 m_SquareColor = {0.2f, 0.3f, 0.8f};
 };
 
 class Sandbox : public Jazz::Application {
