4cab431384f2b407919eb43a974eb496de1ab24e
Vitaly Senokosov <vitalyse@playtika.com>
2021-11-26 09:28:26 +0300

    add blending support, and loading both RGB and RGBA textures

--- Sandbox/src/SandboxGame.cpp
+++ Sandbox/src/SandboxGame.cpp
@@ -161,6 +161,7 @@ public:
         m_TextureShader.reset(Jazz::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
         m_Texture = Jazz::Texture2D::Create("assets/textures/Checkerboard.png");
+        m_ChernoLogoTexture = Jazz::Texture2D::Create("assets/textures/ChernoLogo.png");
 
         std::dynamic_pointer_cast<Jazz::OpenGLShader>(m_TextureShader)->Bind();
         std::dynamic_pointer_cast<Jazz::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -206,6 +207,8 @@ public:
 
         m_Texture->Bind();
         Jazz::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+        m_ChernoLogoTexture->Bind();
+        Jazz::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
         // Triangle
         // Jazz::Renderer::Submit(m_Shader, m_VertexArray);
@@ -229,7 +232,7 @@ private:
     Jazz::Ref<Jazz::Shader> m_FlatColorShader, m_TextureShader;
     Jazz::Ref<Jazz::VertexArray> m_SquareVA;
 
-    Jazz::Ref<Jazz::Texture2D> m_Texture;
+    Jazz::Ref<Jazz::Texture2D> m_Texture, m_ChernoLogoTexture;
 
     Jazz::OrthographicCamera m_Camera;
     glm::vec3 m_CameraPosition;
