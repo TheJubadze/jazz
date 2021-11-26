d94079eb89fd80c098adfd57ec73f8a90aed7851
Vitaly Senokosov <vitalyse@playtika.com>
2021-11-26 11:31:22 +0300

    read shaders from files

--- Jazz/src/Jazz/Renderer/Shader.cpp
+++ Jazz/src/Jazz/Renderer/Shader.cpp
@@ -5,6 +5,19 @@
 
 namespace Jazz {
 
+    Shader *Shader::Create(const std::string &filepath) {
+        switch (Renderer::GetAPI()) {
+            case RendererAPI::API::None:
+                JZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+                return nullptr;
+            case RendererAPI::API::OpenGL:
+                return new OpenGLShader(filepath);
+        }
+
+        JZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+
     Shader *Shader::Create(const std::string &vertexSrc, const std::string &fragmentSrc) {
         switch (Renderer::GetAPI()) {
             case RendererAPI::API::None:
