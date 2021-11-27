064bbefeea55ab3de10c5efa3c55d31ac8d15e41
Vitaly <6545415@gmail.com>
2021-11-27 20:34:25 +0300

    add proper window resize handling

--- Jazz/src/Jazz/Application.h
+++ Jazz/src/Jazz/Application.h
@@ -26,10 +26,12 @@ namespace Jazz {
         inline static Application &Get() { return *s_Instance; }
     private:
         bool OnWindowClose(WindowCloseEvent &e);
+        bool OnWindowResize(WindowResizeEvent& e);
 
         std::unique_ptr<Window> m_Window;
         ImGuiLayer *m_ImGuiLayer;
         bool m_Running = true;
+        bool m_Minimized = false;
         LayerStack m_LayerStack;
 
     private:
