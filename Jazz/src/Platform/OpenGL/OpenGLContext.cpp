d55d9c0a5427400bc9c0828dfaadc30c4faa56eb
Vitaly <6545415@gmail.com>
2021-11-28 12:23:30 +0300

    add check OpenGL version

--- Jazz/src/Platform/OpenGL/OpenGLContext.cpp
+++ Jazz/src/Platform/OpenGL/OpenGLContext.cpp
@@ -6,7 +6,7 @@
 namespace Jazz {
 
     OpenGLContext::OpenGLContext(GLFWwindow *windowHandle)
-            : m_WindowHandle(windowHandle) {
+        : m_WindowHandle(windowHandle) {
         JZ_CORE_ASSERT(windowHandle, "Window handle is null!")
     }
 
@@ -19,6 +19,15 @@ namespace Jazz {
         JZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
         JZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
         JZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+#ifdef JZ_ENABLE_ASSERTS
+        int versionMajor;
+        int versionMinor;
+        glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+        glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+        JZ_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Hazel requires at least OpenGL version 4.5!");
+#endif
     }
 
     void OpenGLContext::SwapBuffers() {
