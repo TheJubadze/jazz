7aac57bddcda658b59e3f4d653fe66bbf9afa752
Vitaly <6545415@gmail.com>
2021-12-05 15:27:39 +0300

    Auto deducing an availabe __FUNCSIG__ definition
    Created `JZ_FUNC_SIG` macro to deduce a valid pretty function name macro as `__FUNCSIG__` isn't available on all compilers

--- Jazz/src/Jazz/Debug/Instrumentor.h
+++ Jazz/src/Jazz/Debug/Instrumentor.h
@@ -110,12 +110,34 @@ namespace Jazz {
     };
 }// namespace Jazz
 
+
 #define JZ_PROFILE 1
 #if JZ_PROFILE
+// Resolve which function signature macro will be used. Note that this only
+// is resolved when the (pre)compiler starts, so the syntax highlighting
+// could mark the wrong one in your editor!
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+        #define JZ_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+        #define JZ_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__FUNCSIG__)
+        #define JZ_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+        #define JZ_FUNC_SIG __FUNCTION__
+    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+        #define JZ_FUNC_SIG __FUNC__
+    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+        #define JZ_FUNC_SIG __func__
+    #elif defined(__cplusplus) && (__cplusplus >= 201103)
+        #define JZ_FUNC_SIG __func__
+    #else
+        #define JZ_FUNC_SIG "JZ_FUNC_SIG unknown!"
+    #endif
+
     #define JZ_PROFILE_BEGIN_SESSION(name, filepath) ::Jazz::Instrumentor::Get().BeginSession(name, filepath)
     #define JZ_PROFILE_END_SESSION() ::Jazz::Instrumentor::Get().EndSession()
     #define JZ_PROFILE_SCOPE(name) ::Jazz::InstrumentationTimer timer##__LINE__(name);
-    #define JZ_PROFILE_FUNCTION() JZ_PROFILE_SCOPE(__FUNCSIG__)
+    #define JZ_PROFILE_FUNCTION() JZ_PROFILE_SCOPE(JZ_FUNC_SIG)
 #else
     #define JZ_PROFILE_BEGIN_SESSION(name, filepath)
     #define JZ_PROFILE_END_SESSION()
