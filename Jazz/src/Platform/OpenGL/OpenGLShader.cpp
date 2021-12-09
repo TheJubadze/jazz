bf12aeaf3686df3b21c3bdad51b6c4b1c6be94d1
Vitaly <6545415@gmail.com>
2021-12-09 20:53:14 +0300

    Add textures to batch renderer

--- Jazz/src/Platform/OpenGL/OpenGLShader.cpp
+++ Jazz/src/Platform/OpenGL/OpenGLShader.cpp
@@ -185,6 +185,10 @@ namespace Jazz {
         UploadUniformInt(name, value);
     }
 
+    void OpenGLShader::SetIntArray(const std::string &name, int *values, uint32_t count) {
+        UploadUniformIntArray(name, values, count);
+    }
+
     void OpenGLShader::SetFloat(const std::string &name, float value) {
         JZ_PROFILE_FUNCTION();
 
@@ -212,6 +216,11 @@ namespace Jazz {
     void OpenGLShader::UploadUniformInt(const std::string &name, int value) {
         GLint location = glGetUniformLocation(m_RendererID, name.c_str());
         glUniform1i(location, value);
+    }
+
+    void OpenGLShader::UploadUniformIntArray(const std::string &name, int *values, uint32_t count) {
+        GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+        glUniform1iv(location, count, values);
     }
 
     void OpenGLShader::UploadUniformFloat(const std::string &name, float value) {
